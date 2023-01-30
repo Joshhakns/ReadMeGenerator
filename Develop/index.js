fd81845a0d9fbbce891aea679cd25316f77477f3
Joshhakns <Joshhawkns@gmail.com>
2023-01-30 17:40:30 -0500

    Main part of the work

--- Develop/index.js
+++ Develop/index.js
@@ -1,92 +1,146 @@
+/* eslint-disable no-console */
 // TODO: Include packages needed for this application
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 
-const fs = require ('fs');
+const fs = require('fs');
 
-const generateMarkdown = require ('./utils/generateMarkdown');
-const { userInfo } = require('os');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-inquirer
-.prompt([
-    {
-        type: 'input',
-        message: 'Link your github username by typing it in!',
-        name: 'Username',
-    },
-    {
-        type: 'input',
-        message: 'What is the name of the repo you are working in?',
-        name: 'Github Repository'
-    },
-    {
-        type: 'input',
-        message: 'What would you like your project title to be?',
-        name: 'Title',
-    },
-    {
-        type: 'input',
-        message: 'List a description of your application.',
-        name: 'Description',
-    },
-    {
-        type: 'input',
-        message: 'Any installation instructions?',
-        name: 'Installation',
-    },
-    {
-        type: 'input',
-        message: 'Any usage instructions?',
-        name: 'Usage'
-    },
-    {
-        type: 'input',
-        message: 'Any other contributors?', 
-        name: 'Contributors'
-    },
-    {
-        type: 'input',
-        message: 'Tests?',
-        name: 'Tests'
-    },
-])
-.then((response) =>
-    response.confirm === response.password
-    ? console.log('Success!')
-    : console.log('Try again.')
-);
+const getLicenseList = require('./utils/generateMarkdown');
+// const { title } = require("process");
+const questions = [
+  {
+    type: 'input',
+    message: 'Link your github username by typing it in!',
+    name: 'Username',
+  },
+  {
+    type: 'input',
+    message: 'What is the name of the repo you are working in?',
+    name: 'Github Repository',
+  },
+  {
+    type: 'input',
+    message: 'What would you like your project title to be?',
+    name: 'Title',
+  },
+  {
+    type: 'input',
+    message: 'List a description of your application.',
+    name: 'Description',
+  },
+  {
+    type: 'input',
+    message: 'Any installation instructions?',
+    name: 'Installation',
+  },
+  {
+    type: 'input',
+    message: 'Any usage instructions?',
+    name: 'Usage',
+  },
+  {
+    type: 'input',
+    message: 'Any other contributors?',
+    name: 'Contributors',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'choose your License',
+    choices: getLicenseList().concat('None'),
+  },
+  {
+    type: 'input',
+    message: 'Tests?',
+    name: 'Tests',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeToFile(fileName, data, err => {
-        if (err) {
-            console.log("Error in trying to write the File! Look at your Index.JS file!")
-        } 
-        else {
-            console.log("readMe file has been generated! Congratulations!")
-        }
-    })
+  try {
+    fs.writeFileSync(fileName, data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
+function getTitleString(answers) {
+  return `# ${answers.Title}\n\n`;
+}
+
+function getDescription(answers) {
+  return `## Description\n\n${answers.Description}\n\n`;
+}
+
+function getInstall(answers) {
+  return `## Installation\n\n${answers.Installation}\n\n`;
+}
+
+function getUsage(answers) {
+  return `## Usage\n\n${answers.Usage}\n\n`;
+}
+
+function credits(answers) {
+  return `## Contributors\n\n${answers.Contributors}\n\n`;
+}
+
+function getTest(answers) {
+  return `## Tests\n\n${answers.Tests}\n\n`;
+}
+
+function tableOfContents() {
+  return `## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)\n\n`;
+}
+
+function getLicenseString(answers) {
+  if (answers.license === 'none') return '';
+  return `## License\n\n License under the ${answers.license} license`;
+}
+
+function generateReadMeString(answers) {
+  return (
+    getTitleString(answers)
+    + generateMarkdown(answers.license)
+    + getDescription(answers)
+    + tableOfContents()
+    + getInstall(answers)
+    + getUsage(answers)
+    + credits(answers)
+    + getTest(answers)
+    + getLicenseString(answers)
+  );
+}
 // TODO: Create a function to initialize app
-function init() 
-try
-    {
-    const responses = inquirer.prompt(questions);
-    console.log("Here is what we found. Please make sure the information is correct: " + responses);
-    
-    const findingInfo = api.getUser(responses);
-    console.log("Your Github info: "+ findingInfo);
-
-    console.log("Generating Readme... this may take some time...");
-    const markdown = generateMarkdown(responses, findingInfo);
-    console.log(markdown);
-
-    writeFileAsync('ExampleREADME.md', markdown);
-
-} catch (error){
-    console.log ("error")
+async function run() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    console.log(answers);
+    console.log(generateReadMeString(answers));
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // Function call to initialize app
-init();
+run();
+// const a = {
+//   Username: "Joshhawkns",
+//   "Github Repository": "ReadmeGenerator",
+//   Title: "ReadmeGenerator",
+//   Description:
+//     "This is a ReadMe generator I am making for my class at the Unviersity of Richmond",
+//   Installation: "None",
+//   Usage: "Don't mess up the code!",
+//   Contributors: "Joshua Thompso9n",
+//   Tests: "none",
+// };
+
+// console.log(generateReadMeString(a));
+// console.log(tableOfContents());
+// console.log(getLicenseList());
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,13 @@
+const licenseList = {
+  "Apache2.0":
+    "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+
+  MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+};
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
@@ -11,10 +19,18 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+module.exports = generateMarkdown;
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return licenseList[data] ? licenseList[data] + "\n\n" : "";
 }
 
-module.exports = generateMarkdown;
+
+module.exports = getLicenseList;
+function getLicenseList(){
+  return Object.keys(licenseList)
+}
+
+
+
+
+
